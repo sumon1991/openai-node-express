2377a3c96e7847d75009382243b51a45d7c8055c
Sumon Sen <Sumon.Sen@edynamic.net>
2023-05-20 16:28:31 +0530

    env package added dotend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const { Configuration, OpenAIApi } = require("openai");
 
